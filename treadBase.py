d82a5d5de92da1fb82b0cb532f23775150a512cd
par_defaut <tbilger24@gmail.com>
2022-11-09 14:23:05 +0100

    test avec 100 thread

--- treadBase.py
+++ treadBase.py
@@ -3,17 +3,25 @@ def task(i):
     print(f"Task {i} starts")
     time.sleep(1)
     print(f"Task {i} ends")
-
+'''start = time.perf_counter()
+t1 = threading.Thread(target=task, args=[1]) # création de la thread
+t1.start() # je démarre la thread
+t2 = threading.Thread(target=task, args=[2])
+t2.start()
+t1.join()  # j'attends la fin de la thread
+t2.join()  # j'attends la fin de la thread
+end = time.perf_counter()
+print(f"Tasks ended in {round(end - start, 2)} second(s)")'''
 def main():
     start = time.perf_counter()
-    t1 = threading.Thread(target=task, args=[1]) # création de la thread
-    t1.start() # je démarre la thread
-    t2 = threading.Thread(target=task, args=[2])
-    t2.start()
-    t1.join()  # j'attends la fin de la thread
-    t2.join()  # j'attends la fin de la thread
+    T = []
+    for i in range(100):
+        T.append(threading.Thread(target=task, args=[i]))
+    for i in range(len(T)):
+        T[i].start()
+    for i in range(len(T)):
+        T[i].join()
     end = time.perf_counter()
     print(f"Tasks ended in {round(end - start, 2)} second(s)")
-
 if __name__=="__main__":
     sys.exit(main())
