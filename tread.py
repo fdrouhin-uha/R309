28ed4d25ec1c9f4d6bd2e84def57d8b24eda0811
par_defaut <tbilger24@gmail.com>
2022-11-09 14:02:58 +0100

    test avec deux fonction version propre

--- tread.py
+++ tread.py
@@ -1,11 +1,18 @@
-import time
+import time,sys
 def task(i):
     print(f"Task {i} starts")
     time.sleep(1)
     print(f"Task {i} ends")
-start = time.perf_counter()
-task(1)
-task(2)
-end = time.perf_counter()
-print(f"Tasks ended in {round(end -start, 2)} second(s)")
+
+
+def main():
+    start = time.perf_counter()
+    task(1)
+    task(2)
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
+
+if __name__=="__main__":
+    sys.exit(main())
+
 
