78041fc2ad3544f0f8105ae0140140be0713e357
par_defaut <tbilger24@gmail.com>
2022-11-10 11:34:24 +0100

    fonctionment client/serv

--- socket/client.py
+++ socket/client.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 import socket
 import sys
 
-host="172.0.0.1"
+host="127.0.0.1"
 port = int(sys.argv[1])
 message= 'test'
 client_socket = socket.socket()
-client_socket.connect(host, port)
+client_socket.connect((host, port))
 client_socket.send(message.encode())
-data =
-client_socket.recv(1024).decode()
+data = client_socket.recv(1024).decode()
+print(data)
 client_socket.close()
--- socket/server.py
+++ socket/server.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 import socket
 import sys
 
-host= "172.0.0.1"
+host= "127.0.0.1"
 port = int(sys.argv[1])
 reply= 'reply'
 
 server_socket = socket.socket()
-server_socket.bind(host, port)
+server_socket.bind((host, port))
 server_socket.listen(1)
 conn, address = server_socket.accept()
 data = conn.recv(1024).decode()
