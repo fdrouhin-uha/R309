307fefdc97572ee8022f5a2eb0bdc814310ac649
par_defaut <tbilger24@gmail.com>
2022-11-12 15:25:50 +0100

    test exception

--- exception/excpetion.py
+++ exception/excpetion.py
@@ -1,6 +1,17 @@
-#!/usr/bin/env python3
+import sys
+#EXERCIE 1 
+def divEntier(x: int, y: int) -> int:
+    if x < y:
+        return 0
+    else:
+        x = x - y
+    return divEntier(x, y) + 1
+
+def main():
+    x= int(input('nombre entier: '))
+    y= int(input('nombre entier: '))
+    divEntier(x,y)
+
+
 if __name__ == '__main__':
-    a = float(input("a: "))
-    b = float(input("b: "))
-    res = a/b
-    print(res)
+    sys.exit(main())
