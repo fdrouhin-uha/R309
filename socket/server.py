b7043952567d8a7282c1d48073fddf2da3574db6
par_defaut <tbilger24@gmail.com>
2022-11-14 11:37:03 +0100

    test

--- socket/server.py
+++ socket/server.py
@@ -5,19 +5,21 @@ import sys
 host= "127.0.0.1"
 port = int(sys.argv[1])
 
-if __name__ == '__main__':
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
+
+server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(1)
+conn, address = server_socket.accept()
+data = conn.recv(1024).decode()
+print(data)
+while data != 'arret':
+    conn.send(data.encode())
     server_socket.listen(1)
-    conn, address = server_socket.accept()
+    conn = server_socket.accept()
     data = conn.recv(1024).decode()
-    while data != 'arret':
-        conn.send(data.encode())
-        server_socket.listen(1)
-        conn = server_socket.accept()
-        data = conn.recv(1024).decode()
-    else:
-        conn.close()
+    print (data)
+else:
+    conn.close()
 
 
 '''server_socket = socket.socket()
