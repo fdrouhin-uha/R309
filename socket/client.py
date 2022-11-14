be01c9332d4673d0665bdac274b3852d7409e740
par_defaut <tbilger24@gmail.com>
2022-11-14 11:36:44 +0100

    test

--- socket/client.py
+++ socket/client.py
@@ -4,11 +4,18 @@ import sys
 
 host="127.0.0.1"
 port = int(sys.argv[1])
-message= 'test'
-
+message= 'hello'
 client_socket = socket.socket()
 client_socket.connect((host, port))
 client_socket.send(message.encode())
 data = client_socket.recv(1024).decode()
 print(data)
-client_socket.close()
+while data != 'bye':
+    massage = str(input('votre message: '))
+    client_socket.send(message.encode())
+    data = client_socket.recv(1024).decode()
+    print(data)
+    if data == 'arret':
+        client_socket.close()
+else:
+    client_socket.close()
