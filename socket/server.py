5857f2fe6c3b2296fd796f83ae21ad899ce22db2
par_defaut <tbilger24@gmail.com>
2022-11-14 11:23:36 +0100

    condition d'ouverture fermeture du serveur

--- socket/server.py
+++ socket/server.py
@@ -4,12 +4,26 @@ import sys
 
 host= "127.0.0.1"
 port = int(sys.argv[1])
-reply= 'reply'
 
-server_socket = socket.socket()
+if __name__ == '__main__':
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    conn, address = server_socket.accept()
+    data = conn.recv(1024).decode()
+    while data != 'arret':
+        conn.send(data.encode())
+        server_socket.listen(1)
+        conn = server_socket.accept()
+        data = conn.recv(1024).decode()
+    else:
+        conn.close()
+
+
+'''server_socket = socket.socket()
 server_socket.bind((host, port))
 server_socket.listen(1)
 conn, address = server_socket.accept()
 data = conn.recv(1024).decode()
 conn.send(reply.encode())
-conn.close()
+conn.close()'''
