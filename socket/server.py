763ed055dcb737684cea22f1ca3a460d05d8d793
par_defaut <tbilger24@gmail.com>
2022-11-15 11:15:57 +0100

    exception

--- socket/server.py
+++ socket/server.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 import socket
 import sys
-
-host= "127.0.0.1"
-port = int(sys.argv[1])
 if __name__ == '__main__':
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-    data=''
     try:
-        while data != 'arret':
-            conn, address = server_socket.accept()
-            data = conn.recv(1024).decode()
-            print(data)
-            conn.send(data.encode())
-            while data != 'bye' and data !='arret':
+        host= "127.0.0.1"
+        port = int(sys.argv[1])
+        server_socket = socket.socket()
+        server_socket.bind((host, port))
+        server_socket.listen(1)
+        data=''
+        try:
+            while data != 'arret':
+                conn, address = server_socket.accept()
                 data = conn.recv(1024).decode()
+                print(data)
                 conn.send(data.encode())
-                print (data)
-            if data == 'bye':
-                conn.send(data.encode())
-                conn.close()
-        
-        conn.send(data.encode())
-        server_socket.close()
-    except ConnectionResetError:
-        print("perte de connexion")
-        conn.close()
+                while data != 'bye' and data !='arret':
+                    data = conn.recv(1024).decode()
+                    conn.send(data.encode())
+                    print (data)
+                if data == 'bye':
+                    conn.send(data.encode())
+                    conn.close()
+            conn.send(data.encode())
+            server_socket.close()
+        except ConnectionResetError:
+            print("perte de connexion")
+            conn.close()
+        except TimeoutError:
+            print("time out")
+            conn.close() 
+        except BrokenPipeError:
+            print("connexion perdue")
+            conn.close()
+            server_socket.close() 
+    except PermissionError:
+        print ("le port n'est pas bon")
+
 
 
 '''server_socket = socket.socket()
