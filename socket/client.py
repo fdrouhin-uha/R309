09146fab67ff8f025535ba48a134683ee6be5652
par_defaut <tbilger24@gmail.com>
2022-11-15 11:15:48 +0100

    exception

--- socket/client.py
+++ socket/client.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 import socket
 import sys
-try :
-    host="127.0.0.1"
-    port = int(sys.argv[1])
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    data=''
-    try:
-        while data != 'bye' and data !='arret' :
-            message = input('votre message: ')
-            client_socket.send(message.encode())
-            data = client_socket.recv(1024).decode()
-            print(data)
-        client_socket.close()
-    except ConnectionResetError:
-        client_socket.close()
-except socket.gaierror:
-    print("le nom de l'hote est invalide")
-except TimeoutError:
-    print("time out")
-    client_socket.close()
-except BrokenPipeError:
-    print("connexion perdue")
-    client_socket.close()
+if __name__ == '__main__':
+    try :
+        host="127.0.0.1"
+        port = int(sys.argv[1])
+        client_socket = socket.socket()
+        client_socket.connect((host, port))
+        data=''
+        try:
+            while data != 'bye' and data !='arret' :
+                message = input('votre message: ')
+                client_socket.send(message.encode())
+                data = client_socket.recv(1024).decode()
+                print(data)
+            client_socket.close()
+        except ConnectionResetError:
+            client_socket.close()
+        except TimeoutError:
+            print("time out")
+            client_socket.close()
+        except BrokenPipeError:
+            print("connexion perdue")
+            client_socket.close()
+    except PermissionError:
+        print ("le port n'est pas bon")
+    except socket.gaierror:
+        print("le nom de l'hote est invalide")
