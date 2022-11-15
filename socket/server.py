e1c96bd69bfe68223a0609e7ceaaede6beb0442d
par_defaut <tbilger24@gmail.com>
2022-11-15 09:20:37 +0100

    modif serv

--- socket/server.py
+++ socket/server.py
@@ -4,20 +4,20 @@ import sys
 
 host= "127.0.0.1"
 port = int(sys.argv[1])
-
-
 server_socket = socket.socket()
 server_socket.bind((host, port))
 server_socket.listen(1)
 conn, address = server_socket.accept()
 data = conn.recv(1024).decode()
 print(data)
-while data != 'arret':
+while data != 'bye' and data != 'arret':
+    data = conn.recv(1024).decode()
+    conn.send(data.encode())
+    print (data)
+if data == 'bye':
     conn.send(data.encode())
     server_socket.listen(1)
-    conn = server_socket.accept()
-    data = conn.recv(1024).decode()
-    print (data)
+    conn, address = server_socket.accept()
 else:
     conn.send(data.encode())
     conn.close()
