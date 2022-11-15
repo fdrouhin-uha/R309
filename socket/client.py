b752c0dc23d6ba5ce4d0303cd37f96b53aaf6165
par_defaut <tbilger24@gmail.com>
2022-11-15 09:20:13 +0100

    modif serv + client

--- socket/client.py
+++ socket/client.py
@@ -4,18 +4,13 @@ import sys
 
 host="127.0.0.1"
 port = int(sys.argv[1])
-message= 'hello'
 client_socket = socket.socket()
 client_socket.connect((host, port))
-client_socket.send(message.encode())
-data = client_socket.recv(1024).decode()
-print(data)
-while data != 'bye':
+data=''
+while data != 'bye' and data !='arret' :
     message = str(input('votre message: '))
     client_socket.send(message.encode())
     data = client_socket.recv(1024).decode()
     print(data)
-if data == 'arret':
-    client_socket.close()
-else:
-    client_socket.close()
+    
+client_socket.close()
