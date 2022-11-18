5a257f9412ab38444aa6f44df39fa09f9d53d1c9
par_defaut <tbilger24@gmail.com>
2022-11-18 16:44:56 +0100

    conversion fonctionnel

--- Gui/exo2.py
+++ Gui/exo2.py
@@ -9,6 +9,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         self.__grid = QGridLayout()
         widget.setLayout(self.__grid)
+        
         self.__lab1 = QLabel("Température")
         self.__text1 = QLineEdit("")
         self.__lab2 = QLabel("°C")
@@ -18,19 +19,35 @@ class MainWindow(QMainWindow):
         self.__lab3 = QLabel("Conversion")
         self.__lab4 = QLabel("")
         self.__lab5 = QLabel("K")
+        self.__help = QPushButton("?")
         self.__selec.addItem("°C -> K")
         self.__selec.addItem("K -> °C")
+        
         self.__grid.addWidget(self.__lab1, 0, 0)
         self.__grid.addWidget(self.__text1, 0, 1)
-        self.__grid.addWidget(self.__lab2, 0, 2)
         self.__grid.addWidget(self.__lab6, 1, 0)
         self.__grid.addWidget(self.__conv, 1, 1)
         self.__grid.addWidget(self.__selec, 1, 2)
         self.__grid.addWidget(self.__lab3, 2, 0)
         self.__grid.addWidget(self.__lab4, 2, 1)
-        self.__grid.addWidget(self.__lab5, 2, 2)
-
-
+        self.__grid.addWidget(self.__help, 3, 3)
+        
+        self.__conv.clicked.connect(self._actionConv)
+        self.__help.clicked.connect(self._actionHelp)
+    
+    
+    def _actionConv(self):
+        if self.__selec.currentText() == "°C -> K":
+            self.__grid.addWidget(self.__lab2, 0, 2)
+            self.__grid.addWidget(self.__lab5, 2, 2)
+            self.__lab4.setText(f"La temperature en Kelvin est {float(self.__text1.text()) + 273.15}")
+        else:
+            self.__lab4.setText(f"La temperature en Celsius est {float(self.__text1.text()) - 273.15}")
+            self.__grid.addWidget(self.__lab2, 2, 2)
+            self.__grid.addWidget(self.__lab5, 0, 2)
+    
+    def _actionHelp():
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
