77bf0f738940bf0ee1ea85923d97d84aba62c278
par_defaut <tbilger24@gmail.com>
2022-11-18 17:00:19 +0100

    msg erreur + aide

--- Gui/exo2.py
+++ Gui/exo2.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QComboBox,QMessageBox
 from PyQt5.QtCore import QCoreApplication
 
 class MainWindow(QMainWindow):
@@ -34,20 +34,33 @@ class MainWindow(QMainWindow):
         
         self.__conv.clicked.connect(self._actionConv)
         self.__help.clicked.connect(self._actionHelp)
-    
+       
+        
+    def BoxInfo(self, message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(message)
+        msg.exec()
     
     def _actionConv(self):
-        if self.__selec.currentText() == "°C -> K":
-            self.__grid.addWidget(self.__lab2, 0, 2)
-            self.__grid.addWidget(self.__lab5, 2, 2)
-            self.__lab4.setText(f"La temperature en Kelvin est {float(self.__text1.text()) + 273.15}")
-        else:
-            self.__lab4.setText(f"La temperature en Celsius est {float(self.__text1.text()) - 273.15}")
-            self.__grid.addWidget(self.__lab2, 2, 2)
-            self.__grid.addWidget(self.__lab5, 0, 2)
+        try:
+            if self.__selec.currentText() == "°C -> K":
+                self.__grid.addWidget(self.__lab2, 0, 2)
+                self.__grid.addWidget(self.__lab5, 2, 2)
+                self.__lab4.setText(f"La temperature en Kelvin est {float(self.__text1.text()) + 273.15}")
+            else:
+                self.__lab4.setText(f"La temperature en Celsius est {float(self.__text1.text()) - 273.15}")
+                self.__grid.addWidget(self.__lab2, 2, 2)
+                self.__grid.addWidget(self.__lab5, 0, 2)
+        except ValueError:
+            self.BoxInfo("rentrez que des entiers")
     
-    def _actionHelp():
-        pass
+    
+    
+    
+    
+    def _actionHelp(self):
+        self.BoxInfo("convertisseur Celsius vers Kalvin ou Kalvin vers Celsius")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
