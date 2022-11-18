e49336d4a72affd1b7b9c098009a02e8f3ad1423
par_defaut <tbilger24@gmail.com>
2022-11-18 15:12:46 +0100

    fin de l'exo 1

--- Gui/exo1.py
+++ Gui/exo1.py
@@ -7,22 +7,25 @@ class MainWindow(QMainWindow):
         super().__init__()
         widget = QWidget()
         self.setCentralWidget(widget)
-        grid = QGridLayout()
-        widget.setLayout(grid)
-        lab = QLabel("Saisir votre nom")
-        text = QLineEdit("")
-        ok = QPushButton("Ok")
-        quit = QPushButton("Quitter")
-        # Ajouter les composants au grid ayout
-        grid.addWidget(lab, 0, 0)
-        grid.addWidget(text, 1, 0)
-        grid.addWidget(ok, 2, 0)
-        ok.clicked.connect(self._actionOk)
-        quit.clicked.connect(self._actionQuitter)
+        self.__grid = QGridLayout()
+        widget.setLayout(self.__grid)
+        self.__lab = QLabel("Saisir votre nom")
+        self.__text = QLineEdit("")
+        self.__affi = QLabel("")
+        self.__ok = QPushButton("Ok")
+        self.__quit = QPushButton("Quitter")
+        self.__grid.addWidget(self.__lab, 0, 0)
+        self.__grid.addWidget(self.__text, 1, 0)
+        self.__grid.addWidget(self.__ok, 2, 0)
+        self.__grid.addWidget(self.__affi, 3, 0)
+        self.__grid.addWidget(self.__quit, 4, 0)
+        self.__ok.clicked.connect(self._actionOk)
+        self.__quit.clicked.connect(self._actionQuitter)
         self.setWindowTitle("Une première fenêtre")
 
     def _actionOk(self):
-        pass
+        self.__affi.setText(f"Yo {self.__text.text()}")
+        
 
 
     def _actionQuitter(self):
