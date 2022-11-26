543f34445ad14fc40383eea58c1f86f9d6dee728
par_defaut <tbilger24@gmail.com>
2022-11-26 13:00:43 +0100

    exe cmd in shell with server

--- SAE/serveur.py
+++ SAE/serveur.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import sys, socket, subprocess, threading
+import sys, socket, subprocess
 # info of the server
-host= ''
+host= "127.0.0.1"
 port= int(sys.argv[1])
 
 #uping the server
@@ -11,26 +11,33 @@ server_s.listen()
 data=''
 if __name__ == '__main__':
     try:
-        while data != 'arret':
-            print("Je suis en attente")
+        while data != 'shut': #"shut" command shut down the server
+            print("waiting !!")
             try:
                 conn, address = server_s.accept()
-                print("Je suis connecté")
+                print("connected !!")
                 data = ''
-                while data != 'bye' and data !='arret':
+                while data != 'bye' and data !='shut': #"bye" command stop the connexion between the server and the client
                     data = conn.recv(1024).decode()
+                    #execute command in the shell using the argument "data"
                     p = subprocess.Popen(data, stdout=subprocess.PIPE, shell=True)
-                    conn.send(data.encode())
-                    print (f"E/R: {data}")
+                    try:
+                        outs, errs = p.communicate(None, 10)
+                    except subprocess.TimeoutExpired:
+                        print(f"Timeout on command {data}")
+                    else:
+                        txt = outs.decode().rstrip("\r\n")
+                        print(txt)
+                        conn.send(txt.encode())
             except ConnectionResetError:
-                print("perte de connexion")
+                print("connexion lost")
             except TimeoutError:
                 print("time out")
             except BrokenPipeError:
-                print("connexion perdue")
+                print("connexion broke")
             finally:
                 conn.close()
     except PermissionError:
-        print ("le port n'est pas bon")
+        print ("connexion port is not correct")
     finally:
         server_s.close()
