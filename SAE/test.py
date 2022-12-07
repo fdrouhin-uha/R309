80b10cb5501fcb607ac8734cfb78ce76fdd6a654
par_defaut <tbilger24@gmail.com>
2022-12-07 14:16:34 +0100

    LA science

--- SAE/test.py
+++ SAE/test.py
@@ -1,46 +1,18 @@
-#!/usr/bin/env python3
-import subprocess,psutil,platform,sys
+import sys 
+import threading
+from guiChat import ChatWindow
+from GUIhome import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QCoreApplication
 
-x = ""
-while x != "bye":
-    x = str(input("commande: "))
-
-    if x != "bye":
-        if x == 'cpu':
-           p = psutil.cpu_percent(interval=1, percpu=True)
-           txt = ', '.join(map(str,p))
-           print(p)
-           print(txt)
-        elif x == "NAME":
-            p = platform.node() 
-            print(p)
-        elif x =="RAM":
-                p = psutil.virtual_memory()
-                print(p)
-        elif x == "OS":
-            if sys.platform == "linux":
-                p = platform.freedesktop_os_release()['PRETTY_NAME'],platform.release()
-                print(p)
-            else:
-                p = platform.system()
-                o = platform.release()
-                print(p,o)
-        elif x =='IP':
-            if sys.platform == 'linux':
-                p = subprocess.Popen("ip a | grep inet | grep global | awk '{print $2}'", stdout=subprocess.PIPE, shell=True)
-                outs = p.communicate()
-                txt = outs.decode().rstrip("\r\n")
-                print(txt)
-            else:
-                p = subprocess.Popen("ipconfig | findstr IPv4", stdout=subprocess.PIPE, shell=True)
-        else:
-            #o = subprocess.check_call('False',shell=False )
-            p = subprocess.Popen(x, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
-            #print(o)
-            try:
-                outs, errs = p.communicate(None, 10)
-            except subprocess.TimeoutExpired:
-                print(f"Timeout on command {x}")
-            else:
-                txt = outs.decode().rstrip("\r\n")
-                print(f'outs = {txt}')
+def home():
+    app = QApplication(sys.argv)
+    win1= MainWindow()
+    win1.show()
+    app.exec()
+    monitor = threading.Thread(target=open)
+    monitor.start()
+def open():
+    win = ChatWindow()
+    win.show()
+home()
