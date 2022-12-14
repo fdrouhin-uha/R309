556b26de301d6ceee25693f514c49d40c063d349
par_defaut <tbilger24@gmail.com>
2022-12-14 14:31:17 +0100

    fix buton

--- SAE/GUIhome.py
+++ SAE/GUIhome.py
@@ -17,8 +17,8 @@ class MainWindow(QMainWindow):
         self.__lab = QLabel("host")
         self.__lab2 = QLabel("port")
         self.__lab3 = QLabel("login history")
-        self.__text = QLineEdit("")
-        self.__text2 = QLineEdit("")
+        self.__text = QLineEdit("127.0.0.1")
+        self.__text2 = QLineEdit("10000")
         self.__conn = QPushButton("connexion")
         self.__affi = QTextBrowser()
         #self.__grid.addWidget(self.__lab3,0,0)
@@ -30,14 +30,14 @@ class MainWindow(QMainWindow):
         self.__grid.addWidget(self.__conn, 4, 1)
         self.__conn.clicked.connect(self.actionConn)
         self.setWindowTitle("home")
-        self.f = open('history.txt','r')
-        self.f.read()
+        #self.f = open('history.txt','r')
+        #self.f.read()
 
     def actionConn(self):
         HOST = self.__text.text()
         p = self.__text2.text()
         PORT = int(p)
-        self.f.write(f"{HOST} / {p}\n")
+        #self.f.write(f"{HOST} / {p}\n")
         diag = ChatWindow(HOST,PORT)
         diag.show()
         diag.exec()
@@ -67,31 +67,32 @@ class ChatWindow(QDialog):
         self.setLayout(self.__grid)
         self.resize(700, 400)
         self.setWindowTitle(f"{HOST}/{PORT}")
-        self.__text = QLineEdit("")
-        self.__quit = QPushButton("DISCONNECT")
-        self.__reset= QPushButton("RESET")
-        self.__kill = QPushButton("KILL")
+        self.__textCmd = QLineEdit("coucou")
+ #       self.__oofffff = QPushButton("DISCONNECT")
+        self.__resetButton= QPushButton("RESET")
+        self.__reset1Button= QPushButton("Disconnect")
+        self.__killButton = QPushButton("KILL")
         self.__affi = QTextBrowser()
-        self.__grid.addWidget(self.__text, 7, 0)
-        #self.__grid.addWidget(self.__quit, 0, 1)   need to be fix 
-        #self.__grid.addWidget(self.__reset, 1, 1)  need to be fix 
-        #self.__grid.addWidget(self.__kill, 2, 1)   need to be fix 
+        self.__grid.addWidget(self.__textCmd, 7, 0)
+        self.__grid.addWidget(self.__reset1Button, 0, 1)   #need to be fix 
+        self.__grid.addWidget(self.__resetButton, 1, 1)  #need to be fix 
+        self.__grid.addWidget(self.__killButton, 2, 1)   #need to be fix 
         self.__grid.addWidget(self.__affi,0,0,6,1)
-        #self.__quit.clicked.connect(self._exit)    need to be fix 
-        #self.__reset.clicked.connect(self._reset)  need to be fix 
-        #self.__kill.clicked.connect(self._kill)    need to be fix 
-        self.__text.returnPressed.connect(self._send)
+        
+        self.__resetButton.clicked.connect(self.__resetFD)  #need to be fix 
+        self.__killButton.clicked.connect(self.__killFD)    #need to be fix 
+        self.__textCmd.returnPressed.connect(self.__sendFD)
         self.__client = Client(HOST,PORT,self.__affi)
-    
 
-    
+        self.__reset1Button.clicked.connect(self.__exitFD)
+        self.__coucou = 1
 
-    def _send(self):
-        msg = self.__text.text()
-        self.__client.send(msg)
-    
-    
-    def _exit(self):
+
+    def __exitFD(self):
+        if self.__coucou == 2:
+            self.__coucou = 1
+            return
+
         box = QMessageBox()
         box.setWindowTitle("Exit ?")
         box.setText("wanna log out ?")
@@ -103,8 +104,9 @@ class ChatWindow(QDialog):
         if ret == QMessageBox.Yes:
             self.__client.send(":disconnect")
             QDialog.exit(0)
+
     
-    def _reset(self):
+    def __resetFD(self):
         box = QMessageBox()
         box.setWindowTitle("reset ?")
         box.setText("reset the server ?")
@@ -117,7 +119,7 @@ class ChatWindow(QDialog):
             self.__client.send(":reset")
             QDialog.exit(0)
     
-    def _kill(self):
+    def __killFD(self):
         box = QMessageBox()
         box.setWindowTitle("kill ?")
         box.setText("kill the server ?")
@@ -129,6 +131,13 @@ class ChatWindow(QDialog):
         if ret == QMessageBox.Yes:
             self.__client.send(":kill")
             QDialog.exit(0)
+
+
+    def __sendFD(self):
+        self.__coucou = 2
+        msg = self.__textCmd.text()
+        self.__client.send(msg)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
