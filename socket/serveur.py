233f20eb0f7b9e292455860cf1904ed0152f4bc5
Frédéric Drouhin <frederic.drouhin@uha.fr>
2023-11-20 09:57:11 +0100

    début des socket

--- socket/serveur.py
+++ socket/serveur.py
@@ -33,13 +33,3 @@ if __name__ == '__main__':
         print ("le port n'est pas bon")
     finally:
         server_socket.close() 
-
-
-
-'''server_socket = socket.socket()
-server_socket.bind((host, port))
-server_socket.listen(1)
-conn, address = server_socket.accept()
-data = conn.recv(1024).decode()
-conn.send(reply.encode())
-conn.close()'''
