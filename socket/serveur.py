7fd297fc7c83787c83648a8cd9d8fab92770f4aa
Frédéric Drouhin <frederic.drouhin@uha.fr>
2023-11-20 09:53:37 +0100

    début des socket

---
+++ socket/serveur.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Examen NE PAS CHANGER !!!!!
+
+import socket
+import sys
+if __name__ == '__main__':
+    try:
+        host= "127.0.0.1"
+        port = 10000
+        server_socket = socket.socket()
+        server_socket.bind((host, port))
+        server_socket.listen(1)
+        data=''
+        while data != 'bye':
+            print("Serveur> Je suis en attente")
+            try:
+                conn, address = server_socket.accept()
+                print(f"Serveur> Je suis connecté avec le client {address}")
+                data = ''
+                while data != 'bye':
+                    data = conn.recv(1024).decode()
+                    print (f"{data}")
+            except ConnectionResetError:
+                print("Serveur> Erreur : perte de connexion")
+            except TimeoutError:
+                print("Serveur> Erreur : time out")
+            except BrokenPipeError:
+                print("Serveur> Erreur : connexion perdue")
+            finally:
+                conn.close()
+    except PermissionError:
+        print ("le port n'est pas bon")
+    finally:
+        server_socket.close() 
+
+
+
+'''server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(1)
+conn, address = server_socket.accept()
+data = conn.recv(1024).decode()
+conn.send(reply.encode())
+conn.close()'''
